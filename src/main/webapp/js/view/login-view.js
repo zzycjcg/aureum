779e139e8f24e6d42324009541ea7723e57f51c7
zhiyong zhu <zzycjcg@qq.com>
2016-04-06 22:30:10 +0800

    调整首页功能

---
+++ src/main/webapp/js/view/login-view.js
@@ -0,0 +1,10 @@
+/*
+	登录按钮颜色渐变
+*/
+$("#login-submit").mouseenter(function() {
+    $(this).attr("style", "background-color: #60b044;");
+});
+
+$("#login-submit").mouseleave(function() {
+    $(this).attr("style", "background-color:  #79d858;");
+});
